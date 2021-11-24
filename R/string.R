1d2e99ea48ac6aab3809ede76f44dbd027426c00
Uchida Mizuki <35052925+UchidaMizuki@users.noreply.github.com>
2021-11-24 23:00:37 +0900

    Create package

---
+++ R/string.R
@@ -0,0 +1,9 @@
+str_to_snakecase <- function(string) {
+  string %>%
+    stringr::str_split("(?=[[:upper:]])") %>%
+    purrr::map_chr(function(string) {
+      string %>%
+        stringr::str_to_lower() %>%
+        stringr::str_c(collapse = "_")
+    })
+}
